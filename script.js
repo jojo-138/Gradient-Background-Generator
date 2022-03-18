eb9f21daa9f0a29e5b3c3320dff68fbb1234dfbd
Jojo <101021415+jojo-138@users.noreply.github.com>
2022-03-17 18:10:42 -0700

    added random color buttons and changed h1 spacing

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-var color1 = document.querySelector('.color1');
-var color2 = document.querySelector('.color2');
-var body = document.getElementById('gradient');
-var h3 = document.querySelector('h3');
+//Match color inputs with background gradient
+
+const color1 = document.querySelector('.color1');
+const color2 = document.querySelector('.color2');
+const body = document.getElementById('gradient');
+const h3 = document.querySelector('h3');
 
 function setGradient() {
 	body.style.background = `linear-gradient(to right, ${color1.value} , ${color2.value})`;
@@ -11,3 +13,41 @@ function setGradient() {
 color1.addEventListener('input', setGradient);
 
 color2.addEventListener('input', setGradient);
+
+//Select random colors
+const random1 = document.getElementById('random1');
+const random2 = document.getElementById('random2');
+
+function randomNum() {
+	return Math.floor(Math.random() * 256);
+}
+
+function numToHex(num) {
+	let hex = num.toString(16);
+	return hex.length === 1 ? '0' + hex : hex;
+}
+
+function rgbToHex(r,g,b) {
+	let hexR = numToHex(r);
+	let hexG = numToHex(g);
+	let hexB = numToHex(b);
+	return '#' + hexR + hexG + hexB;
+}
+
+random1.addEventListener('click', function() {
+	let r = randomNum();
+	let g = randomNum();
+	let b = randomNum();
+	body.style.background = `linear-gradient(to right, rgb(${r},${g},${b}) , ${color2.value})`;
+	color1.value = rgbToHex(r,g,b);
+	h3.textContent = body.style.background + ';';
+});
+
+random2.addEventListener('click', function() {
+	let r = randomNum();
+	let g = randomNum();
+	let b = randomNum();
+	body.style.background = `linear-gradient(to right, ${color1.value} , rgb(${r},${g},${b}))`;
+	color2.value = rgbToHex(r,g,b);
+	h3.textContent = body.style.background + ';';
+});
